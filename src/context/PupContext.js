e0ef9f6795b5ea7d734aeb309c8519b408d3328d
John <jjpyae@gmail.com>
2023-11-17 23:36:07 -0800

    completed practice

--- src/context/PupContext.js
+++ src/context/PupContext.js
@@ -1,18 +1,18 @@
-import { createContext, useState, useContext } from 'react'
+import { createContext, useState, useContext } from 'react';
 import speedy from '../pups/speedy-pup.jpg';
 
 export const PupContext = createContext();
 
-export function PupProvider(props) {
+export const PupProvider = (props) => {
   const [puppyType, setPuppyType] = useState(speedy);
 
   return (
-    <PupContext.Provider value={{ puppyType, setPuppyType }}>
+    <PupContext.Provider value={{puppyType, setPuppyType}}>
       {props.children}
     </PupContext.Provider>
   )
-}
+};
 
-export function usePuppyType() {
+export const usePuppyType = () => {
   return useContext(PupContext);
 }
